0911e3218049bb6625cc36b24f75a8791dc30087
Thales Souza <t.douglas.souza@gmail.com>
2025-09-15 13:31:15 -0300

    produção

--- settings.py
+++ settings.py
@@ -3,8 +3,7 @@ DB_KEY = 'FortbrasRPA'
 DB_ENC = '5QxJNLWe8Aqzn0DFY5npxbNMsVEAnl6ES+Utu6qdDM4='
 DB_USER = 'alvarez'
 DB_SERVER = '10.200.47.189'
-# DB_NAME = 'ORQUESTRADOR'
-DB_NAME = 'TESTE'
+DB_NAME = 'ORQUESTRADOR'
 CNN_STRING = f"Driver={{SQL Server}};Server={DB_SERVER};Database={DB_NAME};Uid={DB_USER};Pwd="
 
 # Parametros do Fluig
